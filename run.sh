155934350e4f335f39e1efbeecac1120578c3477
CloudsWeight <CloudsWeight@users.noreply.github.com>
2023-06-27 21:29:52 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,16 @@ cp kali-logos/* /usr/share/kali-logos/
 
 unzip apple-mac-plymouth.zip
 
-mv apple-mac-plymouth/apple-mac-plymouth macOS
+mv apple-mac-plymouth/macOS
 
-ls 
+cp -r macOS /usr/share/plymouth/themes/.  
+
+echo "Checking if theme copied"
+
+plymouth-set-default-theme -l
+# -R flag rebuilds initrd
+plymouth-set-default-theme -R macOS
+
+echo "If there are no errors please reboot when you can"
+
 
